816b32945bd87e99f8e8892f962f2f69b5163f34
Pedro Sandes <pedrosandes122@gmail.com>
2021-05-25 20:01:06 -0300

    Atribuido a função a constante

--- exer2/idade/app.js
+++ exer2/idade/app.js
@@ -1,15 +1,18 @@
 function calcularIdade(anos, meses, dias) {
   const diasNoAno = 365;
   const diasNoMes = 30;
+  const condicional =
+    typeof anos == 'number' && typeof meses == 'number' && typeof dias == 'number';
 
-  if (typeof anos == 'number' || typeof meses == 'number' || typeof dias == 'number') {
+  if (condicional) {
     diasAno = anos * diasNoAno;
     diasMes = meses * diasNoMes;
     diasTotais = diasAno + diasMes + dias;
-    console.log(`São ${diasTotais} dias de vida.`);
   } else {
     console.log(`Error!`);
   }
+  return diasTotais;
 }
+const totaisDias = calcularIdade(17, 5, 9);
 
-calcularIdade(17, 5, 5);
+console.log(`São ${totaisDias} de vida.`);
